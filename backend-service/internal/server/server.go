00d07895b6799b32cd798eeb34231226e8f0e011
DurlaVerder <vrrrr227@gmail.com>
2025-02-05 15:55:41 +0300

    Added server

--- backend-service/internal/server/server.go
+++ backend-service/internal/server/server.go
@@ -1,1 +1,31 @@
 package server
+
+import (
+	"backend-service/internal/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	handlers handlers.Handlers
+	r        *gin.Engine
+}
+
+func NewServer(handlers handlers.Handlers) *Server {
+	return &Server{
+		handlers: handlers,
+	}
+}
+
+func (s *Server) Run(port string) {
+	s.r = gin.Default()
+
+	s.RegisterRoutes()
+
+	s.r.Run(port)
+}
+
+func (s *Server) RegisterRoutes() {
+	s.r.GET("/pings", s.handlers.HandlerGetAllPing)
+	s.r.POST("/ping/add", s.handlers.HandlerAddPing)
+}
